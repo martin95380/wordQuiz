34c8eedac843ee58c437e2c5ee375951e965b968
martin <m_kahre@live.se>
2015-12-01 15:51:56 +0100

    yolo

---
+++ martin.c
@@ -0,0 +1,22 @@
+﻿#include "martin.h"
+int compWordLetters(char *word, char *letters)
+//int check(char *a, char *b)
+{
+	int wordLen = strlen(word);
+	int numOfLet = strlen(letters);
+	int valid = 0;
+
+	for(int i = 0; i < wordLen; i++)
+	{
+		for(int p = 0; p < numOfLet; p++)
+		{
+			if (word[i] == letters[p])
+			{
+				valid += 1;
+				break;
+			}
+		}
+	}
+	if (valid == wordLen){return 1;}
+	return 0;
+}
---
+++ martin.h
@@ -0,0 +1,16 @@
+﻿#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int compWordLetters(char *word, char *leters);
+
+/*
+deskription:
+	checks if all the letter in a word exists in an array of random letters.
+
+Usage:
+	compWordLetters(word, letters)
+	
+
+*/
